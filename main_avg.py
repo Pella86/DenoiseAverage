2e80041680c914a6258d1488650b1b1f6dc982c9
Pella86 <pigmeo127@gmail.com>
2017-06-21 15:42:12 +0200

    Update before switching branch

--- main_avg.py
+++ main_avg.py
@@ -15,47 +15,47 @@ sys.path.append("./src")
 if __name__ == "__main__":
     print("START AVERAGING SCRIPT")
     
-    pathtodataset = "../../silentcam/dataset31/"    
-#
-#    # matplotlib imports
-#    import matplotlib.pyplot as plt
-#    
-#    # My imports
-#    from AvgFolder_class import AvgFolder
-#
-#    
+    pathtodataset = "../../silentcam/dataset32/"    
 
-#    avg = AvgFolder(pathtodataset)
-#    avg.gather_pictures()
-#    avg.c2gscale()
-#    avg.squareit()
-#    avg.binning(0)    
-#    avg.transpose()
-#    avg.normalize() 
-#    
-#    avg.save_imgs()
-#
-#        
-#    avg.generate_template("UseFirstImage")
-#    avg.save_template()
-#    
-#    avg.template.show_image()
-#    plt.show()  
-#    
-#    avg.template.inspect()
-#    
-#    correlate = True
-#    if correlate:
-#        # aling dataset
-#        avg.align_images()
-#        avg.save_algimgs()
-#        avg.save_corrs()
-#        avg.save_shifts()
-#        
-#        avg.average()
-#        avg.save_avg()
-#        avg.avg.show_image()
-#        plt.show()
+    # matplotlib imports
+    import matplotlib.pyplot as plt
+    
+    # My imports
+    from AvgFolder_class import AvgFolder
+
+    
+
+    avg = AvgFolder(pathtodataset)
+    avg.gather_pictures()
+    avg.c2gscale()
+    avg.squareit()
+    avg.binning(0)    
+    avg.transpose()
+    avg.normalize() 
+    
+    avg.save_imgs()
+
+        
+    avg.generate_template("UseFirstImage")
+    avg.save_template()
+    
+    avg.template.show_image()
+    plt.show()  
+    
+    avg.template.inspect()
+    
+    correlate = True
+    if correlate:
+        # aling dataset
+        avg.align_images()
+        avg.save_algimgs()
+        avg.save_corrs()
+        avg.save_shifts()
+        
+        avg.average()
+        avg.save_avg()
+        avg.avg.show_image()
+        plt.show()
 
     from AvgRGB_class import AvgRGB
     
