2006e71218ba89a712f52de2b3768bb2ac7056ed
Pella86 <pigmeo127@gmail.com>
2017-07-12 15:10:36 +0200

    Added some beauty edits to the report

--- src/reportGenerator.py
+++ src/reportGenerator.py
@@ -6,6 +6,9 @@ Created on Tue Jul  4 12:48:41 2017
 """
 import AvgFolder_class
 
+from LogTimes import Logger
+from pathlib import Path 
+
 class reportGenerator:
 
     def __init__(self, title, path):
@@ -13,11 +16,35 @@ class reportGenerator:
         self.title = title
         self.path = path
         self.avg = AvgFolder_class.AvgFolderMem(self.path)
+        
+        self.mylog = Logger("Report Generator", self.avg.subfolders["results"] + "reportGeneratorlog.txt")
+        
+        # create a dictionary for the table
+        # name of folder : "PathImageArray"
+        self.tabledict = {"aligned_images": self.avg.algimgs, "correlation_images" : self.avg.corrs, "processed_images" : self.avg.imgs, "init_images" : self.avg.init_imgs}
 
+    def create_image_table(self, tablename):
+        #aligned images
+        self.out_file.write('<div class ="' + tablename + '-img ">\n')
+        self.out_file.write("<p>"  + tablename + "</p>\n")
+        
+        patharray = self.tabledict[tablename]
+        
+        for i in range(patharray.n):
+            path_preview = patharray.get_path_to_img(i)
+            print(path_preview)
+            p = Path(path_preview)
+            self.out_file.write("<img src=" + '"' +  str(p.absolute()) + '" style="width:128px;"' + "/" + ">")            
+        
+        self.out_file.write("</div>\n") 
 
     def generate(self):
-
+        
+        self.mylog.log("Report generation started", True)
+        
+        
         self.out_file = open(self.path + "test.html","w")
+        
 
         #create html page
         self.out_file.write("<html>\n<body>\n")
@@ -32,54 +59,35 @@ class reportGenerator:
         #initial images
         self.out_file.write("<div class =\"dataset-img \">\n")
         self.out_file.write("<p>Dataset's photos</p>\n")
-
-        for self.img_path in self.avg.init_imgs.paths:
-            self.out_file.write("<img src=\"" + self.img_path + "\">")
-
-        self.out_file.write("</div>\n")
-
-        #aligned images
-        self.out_file.write("<div class =\"aligned-img \">\n")
-        self.out_file.write("<p>Aligned Images</p>\n")
-
-        for i, aligned_image in enumerate(self.avg.algimgs):
-            self.path_preview = self.avg.subfolders["aligned_images"] + "algimage_" + str(i) + ".png"
-            aligned_image.save(self.path_preview)
-            self.out_file.write("<img src=\"" + self.path_preview + "\">")
-
-        #correlation images
-        self.out_file.write("<div class =\"correlation-img \">\n")
-        self.out_file.write("<p>Correlation Images</p>\n")
-
-        for i, correlation_image in enumerate(self.avg.corrs):
-            self.path_preview = self.avg.subfolders["correlation_images"] + "correlation_" + str(i) + ".png"
-            correlation_image.save(self.path_preview)
-            self.out_file.write("<img src=\"" + self.path_preview + "\">")
-
-        #processed images
-        self.out_file.write("<div class =\"processed-img \">\n")
-        self.out_file.write("<p>Processed Images</p>\n")
-
-        for i, processed_image in enumerate(self.avg.imgs):
-            self.path_preview = self.avg.subfolders["processed_images"] + "proc_imgs_" + str(i) + ".png"
-            processed_image.save(self.path_preview)
-            self.out_file.write("<img src=\"" + self.path_preview + "\">")
-
-        self.out_file.write("</div>\n")
-        self.out_file.write("</div>\n")
-        self.out_file.write("</div>\n")
+        
+        # create the image tables
+        self.mylog.log("Opening initial images")
+        self.create_image_table("init_images")
+        self.create_image_table("aligned_images")
+        self.create_image_table("correlation_images")
+        self.create_image_table("processed_images")
 
         #Template
         self.out_file.write("<p>Template used</p>\n")
-        self.out_file.write("<img src=\"" + self.avg.get_template_path() + "\">")
+        self.out_file.write("<img src=\"" + str(Path(self.avg.get_template_path()).absolute()) + "\">")
         #result
         self.out_file.write("<p>Result</p>\n")
-        self.out_file.write("<img src=\"" + self.avg.get_avg_path() + "\">")
+        self.out_file.write("<img src=\"" + str(Path(self.avg.get_avg_path()).absolute()) + "\">")
 
         #closing file
         self.out_file.write("</div>\n")
         self.out_file.write("</body>\n</html>\n")
 
         self.out_file.close()
+        
+        self.mylog.log("Report generation done.", True)
 
-reportGenerator = reportGenerator("testdataset", "")
+
+
+if __name__ == "__main__":
+    folders = ["dataset41"]
+    
+    for fldr in folders:
+        folder = "../../../silentcam/" + fldr + "/"
+        rGen = reportGenerator(fldr, folder)
+        rGen.generate()
